3b26c969b91a1e732da09c404fb447456ff65cac
haroldmud <haroldmudosa@email.com>
2023-09-10 20:16:59 +0200

    feat: get done with the first sections

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/sections/headers";
+import Hero from "./components/sections/hero";
+
+function App() {
+  return (
+    <section className="">
+      <Header />
+      <section className="bg-slate-100 pb-32">
+        <Hero />
+      </section>
+      <section className="border border-red-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, magni! Ad repellendus atque harum eveniet commodi illum nulla. Officia placeat repudiandae accusantium culpa illo molestiae ad at voluptates? Dolorem, doloremque.
+        Unde tempora beatae nam eum, dignissimos dolor, nesciunt illum consequatur blanditiis nihil quaerat voluptas totam asperiores voluptate perferendis soluta eius! Dolor enim debitis illo doloribus recusandae, omnis ut suscipit quos?
+        Ab nisi quia voluptatibus mollitia fugiat! Explicabo deleniti officiis illum. Fugit voluptate temporibus sequi aspernatur facilis amet dolor consequatur cupiditate ad enim veniam modi maiores quod corrupti, nam sed reiciendis?
+        Impedit sunt voluptatum ducimus. Dicta voluptatibus deserunt enim, aliquid atque quos harum nihil accusamus, est hic asperiores, facere ducimus odit! Animi sed veritatis placeat saepe! Quae laborum enim voluptate. Voluptatem!
+        Iste neque temporibus sint non recusandae impedit, eius, veritatis ad accusamus dicta, odio sunt adipisci sequi error ab minima? Dicta vero corrupti, cupiditate exercitationem voluptatem incidunt voluptates perferendis nihil illum.
+        Corporis dolorum architecto perferendis, accusamus cum perspiciatis suscipit recusandae quidem asperiores quae necessitatibus eius assumenda tenetur officiis provident consequuntur qui aut debitis omnis. Omnis dolorem quasi sed at earum vero?
+        Consequuntur itaque, eaque, fuga est repellendus eius minus qui placeat reprehenderit nostrum, nam veritatis necessitatibus animi sapiente? Maiores nihil ipsa quaerat. Quae adipisci, obcaecati totam facilis aperiam quis tenetur inventore.
+        Nostrum accusantium inventore minus amet explicabo est, deserunt ipsum quas ex tenetur modi expedita aperiam voluptatibus delectus rerum aut. Modi numquam architecto, nihil a ipsam porro obcaecati deleniti dicta error.
+        Omnis consequatur id sint quas magnam quibusdam officiis, et rem odio ipsam dolores provident architecto distinctio? Quibusdam ab laboriosam deserunt assumenda odit. Assumenda illo asperiores eveniet nostrum vel incidunt harum.
+        Quaerat voluptates, tenetur tempore ipsum fugiat accusantium veritatis eveniet libero provident, sed, cupiditate quasi soluta aliquid quisquam itaque dolorum dolores ex illum blanditiis iure tempora asperiores dolore eligendi. Impedit, assumenda?
+        Animi consequatur laborum autem! Explicabo quo voluptas nemo. Et amet, aliquam excepturi reprehenderit temporibus odit nostrum, non explicabo laboriosam vero autem harum illo veritatis deleniti iusto similique libero. Odio, nemo.
+        Odio placeat vero rem sunt repudiandae pariatur iusto numquam id cupiditate dolores dolor quos odit tempore fugiat, harum minima aut provident illo molestias. Pariatur accusantium eos laborum enim vel numquam!
+      </section>
+    </section>
+  );
+}
+
+export default App;
