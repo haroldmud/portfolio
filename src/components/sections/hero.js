3a656198ba8fd3f7a9dbadcffeb71bfc9eeb657b
haroldmud <haroldmudosa@email.com>
2023-09-11 13:23:53 +0200

    feat: nothing for now

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -3,14 +3,14 @@ import { CiLinkedin } from 'react-icons/ci'
 
 export default function Hero() {
   return(
-    <section className="mt-16 max-w-8xl flex flex-col  justify-center mx-auto w-full">
-          <div className="mx-auto flex md:flex-row flex-col-reverse justify-between mt-40 md:gap-0 gap-8 md:px-2 px-4">  
+    <section className="mt-16 max-w-[80rem] flex flex-col  justify-center mx-auto w-full">
+          <div className="flex md:flex-row flex-col-reverse justify-between mt-40 md:gap-0 gap-8 md:px-2 px-4">  
             <div className="md:w-6/12 mx-auto h-fit my-auto flex flex-col gap-8">
               <h1 className="md:text-6xl text-5xl font-bold">Full-Stack Software Developer <span className="inline-block"><img className="w-14" src="/icons/wave.png" alt="" /></span></h1>
               <p className="text-xl">Hi, I'm Arnold Mudosa. A passionate Software Developer based in Kigali, Rwanda.📍</p>
               <div className='flex gap-2'>
-                <a href='https://www.linkedin.com/in/harold-mudosa/' target='blank' className='text-3xl'><CiLinkedin /></a>
-                <a href='https://github.com/haroldmud' target='blank' className='text-2xl my-auto'><LuGithub /></a>
+                <a href='https://www.linkedin.com/in/harold-mudosa/' target='blank' className='hover:text-blue-500 text-3xl'><CiLinkedin /></a>
+                <a href='https://github.com/haroldmud' target='blank' className='hover:text-blue-500 text-2xl my-auto'><LuGithub /></a>
               </div>
             </div>
             <div className="hero-img mx-auto"></div>
